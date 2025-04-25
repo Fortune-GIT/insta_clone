7055621ca196e815b9bd2a6356ceaa3444260a9b
Fortune-GIT <anosike.fortune@gmail.com>
2025-04-25 03:08:54 +0100

    authcontext

--- instagram-clone/src/App.jsx
+++ instagram-clone/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+import Messages from "./pages/Messages";
+import GroupMessages from "./pages/GroupMessages";
+import Notifications from "./pages/Notifications";
+import Saved from "./pages/Saved";
+import EditProfile from "./pages/EditProfile";
+import Reels from "./pages/Reels";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/group" element={<GroupMessages />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/edit" element={<EditProfile />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/reels" element={<Reels />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
-
-export default App
